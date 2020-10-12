38cc1390886467faca25371149f742a7cde7b8c1
christophe gueroult <christophegueroult@gmail.com>
2020-10-12 13:33:30 +0100

    archi avec modules, imports et exports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'estel-crm';
+  public title = 'estel-crm';
 }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginModule } from '../login/login.module';
+import { UiModule } from '../ui/ui.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { IconsModule } from '../icons/icons.module';
+import { TextModule } from '../text/text.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LoginModule,
+    UiModule,
+    TemplatesModule,
+    IconsModule,
+    TextModule
+  ]
+})
+export class CoreModule { }
