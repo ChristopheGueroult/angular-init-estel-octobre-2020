6bd2bd0ff28024ea4ba58dccc27f96457bd367d4
christophe gueroult <christophegueroult@gmail.com>
2020-10-12 14:48:25 +0100

    ng add ng bootstrap et fontawesome + icons comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDeleteComponent } from './icon-delete/icon-delete.component';
+import { IconEditComponent } from './icon-edit/icon-edit.component';
+import { IconNavComponent } from './icon-nav/icon-nav.component';
+import { IconCloseComponent } from './icon-close/icon-close.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IconDeleteComponent, IconEditComponent, IconNavComponent, IconCloseComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [IconDeleteComponent, IconEditComponent, IconNavComponent, IconCloseComponent]
 })
 export class IconsModule { }
